7c03a5d09f224b9738d0074b25694e15e3fec12b
Khoa Thai <thaikhoa2004@gmail.com>
2026-03-30 08:13:46 +0700

    complete backend core business modules

--- backend/src/routes/enrollmentRoutes.js
+++ backend/src/routes/enrollmentRoutes.js
@@ -1,10 +1,21 @@
 import express from 'express';
-import { getEnrollments, createEnrollment, updateEnrollment, deleteEnrollment } from '../controllers/enrollmentController.js';
+import {
+  getEnrollments,
+  createEnrollment,
+  bulkRegisterStudents,
+  bulkUnregisterStudents,
+  saveGrades,
+  updateEnrollment,
+  deleteEnrollment,
+} from '../controllers/enrollmentController.js';
 
 const router = express.Router();
 
 router.get('/', getEnrollments);
 router.post('/', createEnrollment);
+router.post('/bulk-register', bulkRegisterStudents);
+router.post('/bulk-unregister', bulkUnregisterStudents);
+router.put('/grades', saveGrades);
 router.put('/:id', updateEnrollment);
 router.delete('/:id', deleteEnrollment);
 
